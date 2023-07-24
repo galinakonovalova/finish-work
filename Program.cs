ce9989fe5c9c43aa8997596fd3b23289b0108ba4
Galina Konovalova <galina.konovalova85@gmail.com>
2023-07-24 14:19:28 +0300

    Вводим строки с экрана

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] vvodStrings()
+{
+      Console.WriteLine("Введите строки:");
+
+      int count = 0;
+      string[] AS = new string[count];
+      string s;
+      do
+      {
+        s = Console.ReadLine();
+
+        if (s!="")
+        {
+          count++;
+
+          string[] AS2 = new string[count];
+
+          for (int i = 0; i < AS.Length; i++)
+            AS2[i] = AS[i];
+
+          AS2[count - 1] = s;
+
+          AS = AS2;
+        }
+      } while (s != "");
+
+      for (int i = 0; i < AS.Length; i++)
+        Console.WriteLine("AS[{0}] = {1}", i, AS[i]);
+    return AS;
+}
+
+
+string[] A = vvodStrings();
